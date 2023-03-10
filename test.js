2e68f792d871250201edcc1d39df9e8e9cf3de55
minhajul9 <110691447+minhajul9@users.noreply.github.com>
2023-03-10 19:39:23 +0600

    all error cleared

---
+++ test.js
@@ -0,0 +1,16 @@
+// const sum = (p, q) => parseInt(p) + q;
+// const total = sum("h", 1);
+// console.log(total);
+
+// const marks= 60;
+// const resubmit = -5; 
+// console.log(!(marks > 0 || resubmit > 0));
+
+// const unique = array => array.indexOf('21');
+// const array = [2,3,4,5,2];
+// console.log(2 + '3');
+
+// array = 0;
+
+const colors = {mango: 'green', grapes: 'black', organe: 'yellow'};
+console.log(colors[grapes])
